ce767f0839c07e097df8a146a69bdce272255786
root <root@Igor.>
2025-06-28 17:55:37 +0300

    fixed urls

--- wb_parser/urls.py
+++ wb_parser/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path("", views.search_view, name="search_view"),
-    path("dashboard", views.dashboard, name="dashboard")
+    path("dashboard/", views.dashboard, name="dashboard")
 ]
 
 if settings.DEBUG:
